b7a6c828ad74fd3a8a31ffe85c8812e89e9e1652
Tsangyao Chen <tychen742@gmail.com>
2026-05-26 23:00:47 -0500

    committed @ 2026-05-26-230047-0500 by tychen@tymba.local

--- chapters/03_operators/01_operators.c
+++ chapters/03_operators/01_operators.c
@@ -4,7 +4,8 @@
 int main()
 {
     printf("##### 1/5. Arithmetic Operators #####\n");
-    printf("##### + - * / %% ++ -- #####\n");
+    printf("### + - * / %% ++ -- ###\n");
+    printf("num1 = 5, num2 = 3");
     int num1 = 5;
     int num2 = 3;
 
@@ -23,19 +24,25 @@ int main()
     printf("num1 is %i after --num1\n\n", num1);
 
     printf("##### 2/5. Assignment Operators #####\n");
-    printf("##### = += -= *= ... #####\n\n");
+    printf("### = += -= *= ... ###\n\n");
     
     printf("##### 3/5. Comparison Operators #####\n");
-    printf("##### == != > < >= < #####\n");
+    printf("### == != > < >= < ###\n");
     
     printf("### boolean #####\n");
     printf("include: #include <stdbool.h>\n");
-    printf("is 10 > 9 true? (1: true; 0: false):    %d\n", 10 > 9);
+    printf("is 10 > 9 true? (1: true; 0: false):    %d \n", 10 > 9);
     printf(" 10 > 9 is an expression and will evaluate to true or false\n\n");
 
     printf("##### 4/5. Logical Operators #####\n");
-    printf("##### && || ! #####\n\n");
- 
+    printf("### && || ! ###\n\n");
+
+// change a, b to num1 and num2 and find out:
     printf("##### 5/5. Bitwise Operators #####\n");
-    printf("##### ... #####\n\n");
+    printf("### &, |, ^ ###\n\n");
+    int a = 5;
+    int b = 3;
+    printf("a = 5 = 0101 \n");
+    printf("b = 3 = 0011 \n");
+    printf("a & b = %d \n", a & b);
 }
